012a6fb3bda46ee31adceef348aa171f0f5b598f
ssongta <rrgghh@nate.com>
2017-08-04 09:43:56 +0900

    factorial

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+	if( n == 0 ) return 1;
+	else		 return n * factorial(n -1);
+}
+int main()
+{
+	cout << factorial(10) << endl;
+	return 0;
+}
