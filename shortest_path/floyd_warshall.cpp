00230576265cfe2232ed25e47a628fc05b8b35ec
ssong ta <rrgghh@nate.com>
2017-10-31 18:57:29 +0900

    Floyd-warshall알고리즘 두번째

--- shortest_path/floyd_warshall.cpp
+++ shortest_path/floyd_warshall.cpp
@@ -12,13 +12,13 @@ int graph[MAX][MAX] =
   { 3, 7, 4, 2, 0 },
 };
 
-void printSolution(int (*d)[MAX])
+void printSolution(int (*distance)[MAX])
 {
 	for(int i = 0; i < MAX; i++)
 	{
 		for(int j = 0; j < MAX; j++)
 		{
-			cout<<d[i][j]<<" ";
+			cout<<distance[i][j]<<" ";
 		}
 		cout<<endl;
 	}
@@ -26,14 +26,14 @@ void printSolution(int (*d)[MAX])
 }
 void floyd_warshall()
 {
-	int d[MAX][MAX] = {0};
-	int pie[MAX][MAX] = {0};
+	int distance[MAX][MAX] = {0};
+	int path[MAX][MAX] = {0};
 
 	for(int i = 0; i < MAX; i++)
 	{
 		for(int j = 0; j < MAX; j++)
 		{
-			d[i][j] = graph[i][j];			//거리를 의미하는 d값을 graph값으로 초기화 해준다.
+			distance[i][j] = graph[i][j];			//거리를 의미하는 d값을 graph값으로 초기화 해준다.
 		}
 	}
 	for(int k = 0; k < MAX; k++)
@@ -42,15 +42,15 @@ void floyd_warshall()
 		{
 			for(int j = 0; j < MAX; j++)
 			{
-				if(d[i][j] > (d[i][k] + d[k][j]))			//현재 d[i][j]값이 최소가 아닌경우에는 무조건 최소로 맞춰준다.
+				if(distance[i][j] > (distance[i][k] + distance[k][j]))			//현재 d[i][j]값이 최소가 아닌경우에는 무조건 최소로 맞춰준다.
 				{
-					d[i][j] = d[i][k] + d[k][j];
-					pie[i][j] = k;							//직전노드 값
+					distance[i][j] = distance[i][k] + distance[k][j];
+					path[i][j] = k;							//직전노드 값
 				}
 			}
 		}
 	}
-	printSolution(d);
+	printSolution(distance);
 	return;
 }
 int main()
